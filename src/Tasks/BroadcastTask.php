e06146d0faf801be7f319b63d230606764e47679
DillonPMMP <35786890+DillonPMMP@users.noreply.github.com>
2018-01-29 20:39:54 -0600

    Update BroadcastTask.php

--- src/Tasks/BroadcastTask.php
+++ src/Tasks/BroadcastTask.php
@@ -1,1 +1,25 @@
+<?php
 
+namespace Tasks;
+
+use VanillaCore\VanillaCore;
+
+use pocketmine\Server;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\utils\{TextFormat as C, Config};
+class BroadcastTask extends PluginTask{
+	
+	private $plugin;
+	
+	public function __construct(Core $plugin){
+		$this->plugin = $plugin;
+		parent::__construct($plugin);
+	}
+	
+	public  function onRun(int $currentTick){
+        $getmessages = $this->plugin->broadcastcfg->getNested("broadcast.messages");
+        $randomessages = $getmessages[array_rand($getmessages)];
+        $messages = "$randomessages";
+        $this->plugin->getServer()->broadcastMessage("$messages");
+	}
+}
