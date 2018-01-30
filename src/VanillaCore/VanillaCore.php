bb85ec13ca3a7fc28b7378295bcdc9440789f39c
DillonPMMP <35786890+DillonPMMP@users.noreply.github.com>
2018-01-29 20:09:52 -0600

    Add files via upload

--- src/VanillaCore/VanillaCore.php
+++ src/VanillaCore/VanillaCore.php
@@ -1,1 +1,31 @@
+<?php
 
+namespace VanillaCore;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as C;
+
+#Events
+use Events\PlayerDeath;
+use Events\onDropItem;
+
+class VanillaCore extends PluginBase {
+    
+    public function onLoad() {
+        $this->getLogger()->info(C::YELLOW . "VanillaCore Loading!");
+    }
+    
+    public function onEnable() {
+        $this->RegEvents();
+        $this->getLogger()->info(C::GREEN . "VanillaCore Enabled!");
+    }
+    
+    public function onDisable() {
+        $this->getLogger()->info(C::RED . "VanillaCore Disabled!");
+    }
+    
+    public function RegEvents() {
+        $this->getServer()->getPluginManager()->registerEvents(($this->onDropItem = new onDropItem($this)), $this);
+        $this->getServer()->getPluginManager()->registerEvents(($this->PlayerDeath = new PlayerDeath($this)), $this);
+    }
+}
