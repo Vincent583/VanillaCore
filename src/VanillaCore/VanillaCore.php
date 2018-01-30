a089c634d769e94f1cb92d651a4f04a053a349e3
DillonPMMP <35786890+DillonPMMP@users.noreply.github.com>
2018-01-29 20:38:23 -0600

    Update VanillaCore.php

--- src/VanillaCore/VanillaCore.php
+++ src/VanillaCore/VanillaCore.php
@@ -5,6 +5,9 @@ namespace VanillaCore;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat as C;
 
+#Task
+use Tasks\BroadcastTask, ClearlaggTask};
+
 #Commands
 use Commands\ClearInvCommand;
 
@@ -20,6 +23,7 @@ class VanillaCore extends PluginBase {
     
     public function onEnable() {
         $this->RegEvents();
+        $this->RegTasks();
         $this->RegCommands();
         $this->getLogger()->info(C::GREEN . "VanillaCore Enabled!");
     }
@@ -36,4 +40,14 @@ class VanillaCore extends PluginBase {
         $this->getServer()->getPluginManager()->registerEvents(($this->onDropItem = new onDropItem($this)), $this);
         $this->getServer()->getPluginManager()->registerEvents(($this->PlayerDeath = new PlayerDeath($this)), $this);
     }
-}
+    
+    private function RegTask() {
+        $tick = $this->broadcastcfg->getNested("broadcast.tick");
+			$this->getServer()->getScheduler()->scheduleRepeatingTask(new BroadcastTask($this), $tick); #20 = 1 second
+        }
+    
+        $tick = $this->cmdscfg->get("Clearlagg-tick");
+			$this->getServer()->getScheduler()->scheduleRepeatingTask(new ClearlaggTask($this), $tick); #20 = 1 second
+        }
+    }
+}    
