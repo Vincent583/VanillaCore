bb8c5e9f91fd913edd14488db9cc9f275058e099
DillonPMMP <35786890+DillonPMMP@users.noreply.github.com>
2018-01-30 11:32:39 -0600

    Create WearArmour.php

---
+++ src/Events/WearArmour.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Events;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\item\Item;
+use pocketmine\inventory\InventoryBase;
+use pocketmine\Player;
+use pocketmine\Server;
